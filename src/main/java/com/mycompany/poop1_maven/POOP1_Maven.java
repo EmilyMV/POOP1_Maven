d56c2fb6ac2a15781a0eecc90805b6482346e539
Emily Mendoza <112563765+EmilyMV@users.noreply.github.com>
2023-10-13 00:08:09 -0600

    Creación de práctica 1 con Maven

---
+++ src/main/java/com/mycompany/poop1_maven/POOP1_Maven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poop1_maven;
+
+/**
+ *
+ * @author Mendoza Valdez Emily Isabela
+ */
+public class POOP1_Maven {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde POO gpo 8 con Maven");
+    }
+}
